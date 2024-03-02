fad6225fc81b8621d887873408de9c600e5edc92
Sapakuchi2020 <152281596+Sapakuchi2020@users.noreply.github.com>
2024-03-02 14:25:55 +0800

    Add files via upload

---
+++ Assets/JS/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const resumeSection = document.querySelector('.resume');
+    setTimeout(() => {
+        resumeSection.style.opacity = 1;
+    }, 500); 
+});
